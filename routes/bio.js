deeef10be34b1ffa6a44522eeddaee5b5752e29f
ZMiguel Alves <zmiguel.alves@gmail.com>
2017-11-16 20:13:36 +0000

    clean up

--- routes/bio.js
+++ routes/bio.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Biochemical Reactions', bio: true});
+    res.render('bio', { title: 'Biochemical Reactions', bio: true });
 });
 
 
