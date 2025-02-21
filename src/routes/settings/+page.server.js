608ec1acad13dd7b49fd2ed0f2160e8f3bd1607d
ZMiguel Valdiviesso <zmiguel.alves@gmail.com>
2025-02-21 20:03:40 +0100

    use simple values before defaults when separate are not set

--- src/routes/settings/+page.server.js
+++ src/routes/settings/+page.server.js
@@ -10,21 +10,21 @@ export const load = async ({ cookies, platform }) => {
 
 	// Helper function to get settings for a specific type
 	const getSettings = (suffix = '') => ({
-		input: cookies.get(`input${suffix}`) || 'buy',
-		inMarket: cookies.get(`inMarket${suffix}`) || 'Jita',
-		output: cookies.get(`output${suffix}`) || 'sell',
-		outMarket: cookies.get(`outMarket${suffix}`) || 'Jita',
-		brokers: cookies.get(`brokers${suffix}`) || '3',
-		sales: cookies.get(`sales${suffix}`) || '3.6',
-		skill: cookies.get(`skill${suffix}`) || '5',
-		facility: cookies.get(`facility${suffix}`) || 'large',
-		rigs: cookies.get(`rigs${suffix}`) || '2',
-		space: cookies.get(`space${suffix}`) || 'nullsec',
-		system: cookies.get(`system${suffix}`) || 'Ignoitton',
-		tax: cookies.get(`indyTax${suffix}`) || '1',
-		scc: cookies.get(`sccTax${suffix}`) || '4',
-		duration: cookies.get(`duration${suffix}`) || '10080',
-		costIndex: cookies.get(`costIndex${suffix}`) || '0'
+		input: cookies.get(`input${suffix}`) || cookies.get('input') || 'buy',
+		inMarket: cookies.get(`inMarket${suffix}`) || cookies.get('inMarket') || 'Jita',
+		output: cookies.get(`output${suffix}`) || cookies.get('output') || 'sell',
+		outMarket: cookies.get(`outMarket${suffix}`) || cookies.get('outMarket') || 'Jita',
+		brokers: cookies.get(`brokers${suffix}`) || cookies.get('brokers') || '3',
+		sales: cookies.get(`sales${suffix}`) || cookies.get('sales') || '3.6',
+		skill: cookies.get(`skill${suffix}`) || cookies.get('skill') || '5',
+		facility: cookies.get(`facility${suffix}`) || cookies.get('facility') || 'large',
+		rigs: cookies.get(`rigs${suffix}`) || cookies.get('rigs') || '2',
+		space: cookies.get(`space${suffix}`) || cookies.get('space') || 'nullsec',
+		system: cookies.get(`system${suffix}`) || cookies.get('system') || 'Ignoitton',
+		tax: cookies.get(`indyTax${suffix}`) || cookies.get('indyTax') || '1',
+		scc: cookies.get(`sccTax${suffix}`) || cookies.get('sccTax') || '4',
+		duration: cookies.get(`duration${suffix}`) || cookies.get('duration') || '10080',
+		costIndex: cookies.get(`costIndex${suffix}`) || cookies.get('costIndex') || '0'
 	});
 
 	return {
