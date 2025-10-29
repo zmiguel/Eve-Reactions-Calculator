fab495040c6116477fa0785fdf431641197dc910
José Valdiviesso <zmiguel.alves@gmail.com>
2025-10-29 16:33:07 +0100

    bite 7

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
 	css: {
 		preprocessorOptions: {
 			scss: {
-				additionalData: '@use "src/variables.scss" as *;'
+				additionalData: '@use "variables.scss" as *;\n',
+				loadPaths: ['src']
 			}
 		}
 	}
